a0341fdcdb1c595351502d1a2d21539ec553bf73
shui91 <sherman.sy.hui@gmail.com>
2015-07-11 13:47:54 -0700

    feat: improved jQuery variables to be cleaner and more understandable. removed console.log(bgImg)

--- js/script.js
+++ js/script.js
@@ -6,15 +6,13 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
-    var $streetInput = $('#street');
-    var $cityInput = $('#city');
-    var bgLocation = $streetInput.val() + ', ' + $cityInput.val();
+    var streetInput = $('#street').val();
+    var cityInput = $('#city').val();
+    var bgLocation = streetInput + ', ' + cityInput;
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
-
-    console.log(bgLocation);
 
     // load streetview
     $body.append('<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + bgLocation + '">');
