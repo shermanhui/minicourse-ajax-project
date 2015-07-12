57ea0f39ed2dc42a66c09f99cbacba72970a1fee
shui91 <sherman.sy.hui@gmail.com>
2015-07-11 22:04:58 -0700

    feat: nyTimes API added and now pulls data. (API Key Removed for privacy reasons). todo: still need to append information into list format

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function loadData() {
 
     var $body = $('body');
@@ -9,6 +8,8 @@ function loadData() {
     var streetInput = $('#street').val();
     var cityInput = $('#city').val();
     var bgLocation = streetInput + ', ' + cityInput;
+    var streetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + bgLocation + '';
+    var nyTimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + bgLocation + '&sort=newest&api-key=';
 
     // clear out old data before new request
     $wikiElem.text("");
@@ -17,8 +18,14 @@ function loadData() {
     $greeting.text("Is " + bgLocation + " where you want to live?")
 
     // load streetview
-    $body.append('<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + bgLocation + '">');
-    // YOUR CODE GOES HERE!
+    $body.append('<img class="bgimg" src="' + streetViewURL + '">');
+    //$body.append('<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + bgLocation + '">');
+
+    $.getJSON(nyTimesURL, function(data) {
+        console.log(data);
+        //console.log("http://api.nytimes.com/svc/search/v2/articlesearch.json?q='" + bgLocation + "'&fq=source:('The New York Times')&glocations.contains='" + bgLocation + "'&begin_date=2014101&end_date201507011&sort=newest&api-key=b2db8097cb8fbf02d5f3db8bf759b051:0:72487022");
+    });
+
 
     return false;
 };
